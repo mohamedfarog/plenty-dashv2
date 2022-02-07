030d4009740640f8df6492549aacaff08101d84a
alihassan <alihassan.workmail@gmail.com>
2022-02-07 04:00:14 -0800

    Update login pgae

--- src/views/pages/account/login.vue
+++ src/views/pages/account/login.vue
@@ -4,7 +4,7 @@ import { required, email } from "vuelidate/lib/validators";
 import {
   authMethods,
   authFackMethods,
-  notificationMethods
+  notificationMethods,
 } from "@/state/helpers";
 
 export default {
@@ -12,20 +12,20 @@ export default {
     return {
       email: "admin@themesdesign.in",
       password: "123456",
-      submitted: false
+      submitted: false,
     };
   },
   computed: {
     notification() {
       return this.$store ? this.$store.state.notification : null;
-    }
+    },
   },
   created() {
     document.body.classList.add("auth-body-bg");
   },
   validations: {
     email: { required, email },
-    password: { required }
+    password: { required },
   },
   methods: {
     ...authMethods,
@@ -48,10 +48,10 @@ export default {
           return (
             this.logIn({
               email: this.email,
-              password: this.password
+              password: this.password,
             })
               // eslint-disable-next-line no-unused-vars
-              .then(token => {
+              .then((token) => {
                 this.tryingToLogIn = false;
                 this.isAuthError = false;
                 // Redirect to the originally requested page, or to the home page
@@ -59,7 +59,7 @@ export default {
                   this.$route.query.redirectFrom || { name: "home" }
                 );
               })
-              .catch(error => {
+              .catch((error) => {
                 this.tryingToLogIn = false;
                 this.authError = error ? error : "";
                 this.isAuthError = true;
@@ -72,8 +72,8 @@ export default {
           }
         }
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
@@ -87,33 +87,48 @@ export default {
     <div>
       <div class="container-fluid p-0">
         <div class="row no-gutters">
-          <div class="col-lg-4">
-            <div class="authentication-page-content p-4 d-flex align-items-center min-vh-100">
+          <div class="col-lg-6">
+            <div class="authentication-bg">
+              <div class="login-logo-wrapper">
+                <img
+                  class="plenty-logo"
+                  src="@/assets/plenty/logo.png"
+                  alt=""
+                  srcset=""
+                />
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div
+              class="
+                authentication-page-content
+                p-4
+                d-flex
+                align-items-center
+                min-vh-100
+              "
+            >
               <div class="w-100">
                 <div class="row justify-content-center">
                   <div class="col-lg-9">
-                    <div>
-                      <div class="text-center">
-                        <div>
-                          <a href="/" class="logo">
-                            <img src="@/assets/images/logo-dark.png" height="20" alt="logo" />
-                          </a>
-                        </div>
-
-                        <h4 class="font-size-18 mt-4">Welcome Back !</h4>
-                        <p class="text-muted">Sign in to continue to Nazox.</p>
-                      </div>
-
+                    <div class="login-form">
                       <b-alert
                         variant="danger"
-                        class="mt-3"
+                        class=""
                         v-if="notification.message"
                         show
                         dismissible
-                      >{{notification.message}}</b-alert>
+                        >{{ notification.message }}</b-alert
+                      >
 
-                      <div class="p-2 mt-5">
-                        <form class="form-horizontal" @submit.prevent="tryToLogIn">
+                      <div class="p-1">
+                        <h4 class="font-size-18">Dashboard</h4>
+                        <p class="text-muted">Please Sign in to continue</p>
+                        <form
+                          class="form-horizontal"
+                          @submit.prevent="tryToLogIn"
+                        >
                           <div class="form-group auth-form-group-custom mb-4">
                             <i class="ri-mail-line auti-custom-input-icon"></i>
                             <label for="email">Email</label>
@@ -123,16 +138,27 @@ export default {
                               class="form-control"
                               id="email"
                               placeholder="Enter email"
-                              :class="{ 'is-invalid': submitted && $v.email.$error }"
+                              :class="{
+                                'is-invalid': submitted && $v.email.$error,
+                              }"
                             />
-                            <div v-if="submitted && $v.email.$error" class="invalid-feedback">
-                              <span v-if="!$v.email.required">Email is required.</span>
-                              <span v-if="!$v.email.email">Please enter valid email.</span>
+                            <div
+                              v-if="submitted && $v.email.$error"
+                              class="invalid-feedback"
+                            >
+                              <span v-if="!$v.email.required"
+                                >Email is required.</span
+                              >
+                              <span v-if="!$v.email.email"
+                                >Please enter valid email.</span
+                              >
                             </div>
                           </div>
 
                           <div class="form-group auth-form-group-custom mb-4">
-                            <i class="ri-lock-2-line auti-custom-input-icon"></i>
+                            <i
+                              class="ri-lock-2-line auti-custom-input-icon"
+                            ></i>
                             <label for="userpassword">Password</label>
                             <input
                               v-model="password"
@@ -140,53 +166,34 @@ export default {
                               class="form-control"
                               id="userpassword"
                               placeholder="Enter password"
-                              :class="{ 'is-invalid': submitted && $v.password.$error }"
+                              :class="{
+                                'is-invalid': submitted && $v.password.$error,
+                              }"
                             />
                             <div
                               v-if="submitted && !$v.password.required"
                               class="invalid-feedback"
-                            >Password is required.</div>
-                          </div>
-
-                          <div class="custom-control custom-checkbox">
-                            <input
-                              type="checkbox"
-                              class="custom-control-input"
-                              id="customControlInline"
-                            />
-                            <label
-                              class="custom-control-label"
-                              for="customControlInline"
-                            >Remember me</label>
+                            >
+                              Password is required.
+                            </div>
                           </div>
 
                           <div class="mt-4 text-center">
                             <button
-                              class="btn btn-primary w-md waves-effect waves-light"
+                              class="
+                                btn btn-primary
+                                w-md
+                                waves-effect waves-light
+                                login-btn
+                              "
                               type="submit"
-                            >Log In</button>
-                          </div>
-
-                          <div class="mt-4 text-center">
-                            <router-link tag="a" to="/forgot-password" class="text-muted">
-                              <i class="mdi mdi-lock mr-1"></i> Forgot your password?
-                            </router-link>
+                            >
+                              Log In
+                            </button>
                           </div>
                         </form>
-                      </div>
-
-                      <div class="mt-5 text-center">
-                        <p>
-                          Don't have an account ?
-                          <router-link
-                            tag="a"
-                            to="/register"
-                            class="font-weight-medium text-primary"
-                          >Register</router-link>
-                        </p>
-                        <p>
-                          © 2020 Nazox. Crafted with
-                          <i class="mdi mdi-heart text-danger"></i> by Themesdesign
+                        <p class="mt-4 text-center">
+                          © 2022 Plentyofthings.com
                         </p>
                       </div>
                     </div>
@@ -195,13 +202,19 @@ export default {
               </div>
             </div>
           </div>
-          <div class="col-lg-8">
-            <div class="authentication-bg">
-              <div class="bg-overlay"></div>
-            </div>
-          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.login-form {
+  max-width: 380px !important;
+  margin: auto;
+}
+.login-btn{
+  width: 100%;
+  border-radius: 18px;
+}
+</style>
